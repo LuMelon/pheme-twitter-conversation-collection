9635effc0e723de61deb2bd07cca13f5e9b10ee9
LuMelon <lumenglong2017@gmail.com>
2019-10-23 10:31:53 +0800

    纠正了tweepy无法运行的问题

--- retrieve.tweet.list.py
+++ retrieve.tweet.list.py
@@ -28,3 +28,45 @@ try:
 except:
 	raise
 	sys.exit()
+
+
+
+import json
+import sys
+import pprint
+import os
+import configparser
+import time
+import json
+from twython import Twython
+import os
+import numpy as np
+import random
+import time
+import pickle
+
+tweetids = sys.argv[1]
+idlist = tweetids.strip().split(",")
+# config = configparser.ConfigParser()
+# config.read('twitter.ini')
+# consumer_key = config.get('Twitter', 'consumer_key')
+# consumer_secret = config.get('Twitter', 'consumer_secret')
+# access_key = config.get('Twitter', 'access_key')
+# access_secret = config.get('Twitter', 'access_secret')
+# twitter = Twython(
+#         consumer_key, consumer_secret,
+#         access_key, access_secret
+#     )
+with open("twitter.handler", "rb") as fr:
+    twitter = pickle.load(fr)
+
+
+try:
+	tweets = [twitter.show_status(id=tweetid) for tweetid in idlist]
+	print('\n'.join([json.dumps(tweet) for tweet in tweets]))
+	# with open("test.json", "r") as fr:
+	# 	dic = json.load(fr)
+	# print(json.dumps(dic))
+except:
+	print("Error")
+	sys.exit()
